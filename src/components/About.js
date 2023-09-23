2073c08bb0e6bc6655468ddae052def3ef3cdb25
minkiko@gmail.com <minkiko@gmail.com>
2023-09-23 12:08:12 -0500

    created function for about.js

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium">Full Stack Developer</p>
+      <p className="content">
+        My name is Aaron Ko and I am a cohort of the April 2023 UCLA Coding Bootcamp. I received my Bachelors in Chemistry from the University of Texas at Austin.
+        I am currently working for a Swiss-based company working as a Field Chemist.
+        I am wanting to become a Full Stack Developer for a large Technology Company.           I am excited to work on various projects to develop applications that have a dynamic range of UI/UX elements.
+      </p>
+    </div>
+  );
+}
+
+export default About;
